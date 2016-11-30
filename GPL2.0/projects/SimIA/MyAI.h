aca20448c42621279f7e5ad3dd4dad1a5e2f6682
luannesi <luannesi@gmail.com>
2016-11-30 09:50:42 -0200

    Sensoriamento do BOT SimIA Project

--- GPL2.0/projects/SimIA/MyAI.h
+++ GPL2.0/projects/SimIA/MyAI.h
@@ -1,8 +1,78 @@
 #pragma once
 #include <SimIA>
+#include <ctime>
 using namespace GAMES;
 
 string name = "Luan";
+
+struct Nodo
+{
+
+	vector<Nodo*> *filhos;
+	Nodo *pai;
+	Nodo()
+	{
+		pai = NULL;
+		filhos = new vector<Nodo*>();
+		//filhos->push_back(this);
+	}
+	Nodo(Nodo* pai)
+	{
+		this->pai = pai;
+		filhos = new vector<Nodo*>();
+
+		//filhos->push_back(this);
+		state = 7;
+	}
+	//Nodo *pai;
+	int state;
+	int direcao;
+	int qtd = 0;
+	void addFilhos(int estado, int dir)
+	{
+		qtd++;
+		filhos->push_back(new Nodo(this));
+		filhos->back()->state = estado;
+		filhos->back()->direcao = dir;
+	}
+	//int direcao = -1;
+	int getDirection()
+	{
+		//while (filhos->back()->state == WALL)
+		//{
+		//	filhos->pop_back();
+		//}
+		if (filhos == NULL)
+			return 0;
+		
+		if (filhos->size() == 0)
+		{
+			return 0;
+		}
+
+		if (filhos->back()->direcao == filhos->size() - 1)
+			return filhos->size() + 1;
+
+		return filhos->size();
+		//return filhos->size() - 1;
+		//if (filhos->size() - 1 < 0)
+		//{
+		//	return qtd;
+		//}
+		//return filhos->size() - 1;
+		//direcao++;
+		//if (direcao > 3)
+		//{
+		//	while (filhos.size() > 1)
+		//	{
+		//		filhos.pop_back();
+		//	}
+		//	filhos[0]->state = GROUND;
+		//	direcao = 0;
+		//}
+		//return direcao;
+	}
+};
 
 struct Mapa
 {
@@ -10,13 +80,17 @@ struct Mapa
 	int y = 1;
 	vector<vector<int>> mapa;
 
-	void addLeftColumn(int xx)
+	void addLeftColumn(int &xx)
 	{
 		if (xx == 0)
 		{
 			// Coluna Inicio
 			//7 é o não visto
 			mapa.emplace(mapa.begin(), vector<int>(mapa.back().size(), 7));
+			// todos os bots devem ser deslocados
+			//TODO Bots
+
+			xx++;
 			x++;
 		}
 	}
@@ -28,11 +102,12 @@ struct Mapa
 			mapa.emplace(mapa.end(), vector<int>(mapa.back().size(), 7));
 		}
 	}
-	void addTopRow(int yy)
+	void addTopRow(int &yy)
 	{
 		if (yy == 0)
 		{
 			y++;
+			yy++;
 			// Linha acima
 			for (int i = 0; i < mapa.size(); i++)
 			{
@@ -101,42 +176,52 @@ public:
 		this->ID = ID;
 	}
 
-	void moveUp()
+	bool moveUp()
 	{
 		// se pode mover para cima
 		if (sistema->move(name, ID, DIRECTION::UP_))
 		{
-			y--;
+
 			// inserir linha acima
 			Visao.addTopRow(y);
+			y--;
+			return true;
 		}
+		return false;
 	}
-	void moveDown()
+	bool moveDown()
 	{
 		if (sistema->move(name, ID, DIRECTION::DOWN_))
 		{
-			y++;
 			//Visao.y--;
 			Visao.addBottomRow(y);
+			y++;
+			return true;
 		}
+		return false;
 	}
-	void moveLeft()
+	bool moveLeft()
 	{
 		if (sistema->move(name, ID, DIRECTION::LEFT_))
 		{
-			if(x > 0) x--;
+			//if(x > 0) x--;
 			//Visao.x--;
 			Visao.addLeftColumn(x);
+			x--;
+			return true;
 		}
+		return false;
 	}
-	void moveRight()
+	bool moveRight()
 	{
 		if (sistema->move(name, 0, DIRECTION::RIGHT_))
 		{
-			x++;
 			//Visao.x++;
 			Visao.addRightColumn(x);
+			x++;
+			return true;
 		}
+		return false;
 	}
 };
 
@@ -146,6 +231,8 @@ class MyAI : public AI
 {
 public:
 	Text texto;
+	Nodo *arvore;
+	Nodo *nodoGuia;
 
 	vector<Bot*> *Bots;
 
@@ -159,34 +246,54 @@ public:
 		for each (Bot *b in *Bots)
 		{
 			vector<vector<int>> v = sistema->whereAmI(name, b->ID);
+			if (b->x - 1 >= 0 && b->y - 1 >= 0)
+				Visao.mapa[b->x - 1][b->y - 1] = v[0][0];
+			if (b->x - 1 >= 0)
+				Visao.mapa[b->x - 1][b->y] = v[1][0];
+			if (b->x - 1 >= 0 && b->y + 1 < Visao.mapa[0].size())
+				Visao.mapa[b->x - 1][b->y + 1] = v[2][0];
 
-			Visao.mapa[b->x][b->y]		   = v[0][0];
-			Visao.mapa[b->x][b->y + 1]	   = v[0][1];
-			Visao.mapa[b->x][b->y + 2]	   = v[0][2];
+			if (b->y - 1 >= 0)
+				Visao.mapa[b->x][b->y - 1] = v[0][1];
+			Visao.mapa[b->x][b->y] = v[1][1];
+			if (b->y + 1 < Visao.mapa[0].size())
+				Visao.mapa[b->x][b->y + 1] = v[2][1];
 
-			Visao.mapa[b->x + 1][b->y]	   = v[1][0];
-			Visao.mapa[b->x + 1][b->y + 1] = v[1][1];
-			Visao.mapa[b->x + 1][b->y + 2] = v[1][2];
-
-			Visao.mapa[b->x + 2][b->y]	   = v[2][0];
-			Visao.mapa[b->x + 2][b->y + 1] = v[2][1];
-			Visao.mapa[b->x + 2][b->y + 2] = v[2][2];
+			if (b->x + 1 < Visao.mapa.size() && b->y - 1 >= 0)
+				Visao.mapa[b->x + 1][b->y - 1] = v[0][2];
+			if (b->x + 1 < Visao.mapa.size())
+				Visao.mapa[b->x + 1][b->y] = v[1][2];
+			if (b->x + 1 < Visao.mapa.size() && b->y + 1 < Visao.mapa[0].size())
+				Visao.mapa[b->x + 1][b->y + 1] = v[2][2];
 
 		}
 	}
 
 	int ID_counter = 0;
+	Nodo *semente;
 	void setup()
 	{
 		Visao = Mapa(name);
 		texto.load("calibri.ttf");
 		Bots = new vector<Bot*>;
-		Bots->push_back(new Bot(0, 0, 0));
+		Bots->push_back(new Bot(1, 1, 0));
 		ID_counter++;
+		srand(time(0));
+
+		// Define o nodo da arvore como ja visitado
+//		nodoGuia->addFilhos();
+		semente = new Nodo();
+		arvore = new Nodo(semente);
+		arvore->state = Visao.mapa[1][1];
+
+		nodoGuia = arvore->pai;
 	}
 
 	void run()
 	{
+		//		sensoriamento();
+
+
 		if (Bots->size() < sistema->getPopSize(name))
 		{
 			Bots->push_back(new Bot(1, 1, ID_counter));
@@ -195,26 +302,113 @@ public:
 
 		Visao.drawMap();
 
-		//sensoriamento();
-		//texto.draw("(" + to_string(Visao.x) + "," + to_string(Visao.y) + ")", 450, 500, 30);
+		sensoriamento();
+		texto.draw("(" + to_string(Bots->at(0)->x) + "," + to_string(Bots->at(0)->y) + ")", 450, 500, 30);
 
 		//system("cls");
 		//cout << Visao.x << "," << Visao.y << endl;
-		if (input->isUp(KEY::Up))
+		if (nodoGuia->filhos->size() > 0)
 		{
-			Bots->at(0)->moveUp();
+			if (nodoGuia->filhos->back()->state != WALL)
+				nodoGuia = nodoGuia->filhos->back();
 		}
-		if (input->isUp(KEY::Down))
+		//else
+		//{
+		//	//nodoGuia = nodoGuia->pai;
+		//}
+
+		int dir = nodoGuia->getDirection();
+
+		if (dir == nodoGuia->filhos->size())
 		{
-			Bots->at(0)->moveDown();
+			if (nodoGuia->pai != NULL)
+			{
+				Nodo *nodoTemp = nodoGuia->pai;
+				nodoGuia = nodoTemp;
+				dir = nodoGuia->getDirection();
+			}
 		}
-		if (input->isUp(KEY::Left))
+
+		//int dir = rand() % 4;
+		if (input->isUp(KEY::Up) || dir == 0)
 		{
-			Bots->at(0)->moveLeft();
+			if (nodoGuia->filhos->size() < 5)
+			{
+				nodoGuia->addFilhos(Visao.mapa[Bots->at(0)->x][Bots->at(0)->y], 0);
+				if (Bots->at(0)->moveUp())
+				{
+					nodoGuia->state = Visao.mapa[Bots->at(0)->x][Bots->at(0)->y];
+				}
+				else
+				{
+					nodoGuia->filhos->back()->state = WALL;
+				}
+			}
+			else
+			{
+				Nodo *nodoTemp = nodoGuia->pai;
+				nodoGuia = nodoTemp;;
+			}
 		}
-		if (input->isUp(KEY::Right))
+		else if (input->isUp(KEY::Down) || dir == 1)
 		{
-			Bots->at(0)->moveRight();
+			if (nodoGuia->filhos->size() < 5)
+			{
+				nodoGuia->addFilhos(Visao.mapa[Bots->at(0)->x][Bots->at(0)->y], 0);
+				if (Bots->at(0)->moveDown())
+				{
+					nodoGuia->state = Visao.mapa[Bots->at(0)->x][Bots->at(0)->y];
+				}
+				else
+				{
+					nodoGuia->filhos->back()->state = WALL;
+				}
+			}
+			else
+			{
+				Nodo *nodoTemp = nodoGuia->pai;
+				nodoGuia = nodoTemp;;
+			}
+		}
+		else if (input->isUp(KEY::Left) || dir == 2)
+		{
+			if (nodoGuia->filhos->size() < 5)
+			{
+				nodoGuia->addFilhos(Visao.mapa[Bots->at(0)->x][Bots->at(0)->y], 0);
+				if (Bots->at(0)->moveLeft())
+				{
+					nodoGuia->state = Visao.mapa[Bots->at(0)->x][Bots->at(0)->y];
+				}
+				else
+				{
+					nodoGuia->filhos->back()->state = WALL;
+				}
+			}
+			else
+			{
+				Nodo *nodoTemp = nodoGuia->pai;
+				nodoGuia = nodoTemp;;
+			}
+		}
+		else if (input->isUp(KEY::Right) || dir == 3)
+		{
+			if (nodoGuia->filhos->size() < 5)
+			{
+				nodoGuia->addFilhos(Visao.mapa[Bots->at(0)->x][Bots->at(0)->y], 0);
+				if (Bots->at(0)->moveRight())
+				{
+					nodoGuia->state = Visao.mapa[Bots->at(0)->x][Bots->at(0)->y];
+				}
+				else
+				{
+					nodoGuia->filhos->back()->state = WALL;
+				}
+			}
+			else
+			{
+				Nodo *nodoTemp = nodoGuia->pai;
+				nodoGuia = nodoTemp;;
+			}
 		}
 		//panel->debug("PopSize", sistema->getPopSize(name));
 		//panel->debug("FOOD", sistema->getInfo(FOOD, name, 0));
